5c968c7845d62c9033af4daea2cef32e3a6d913b
Jhe <jeremiahpasion56@gmail.com>
2024-02-26 22:25:12 +0800

    add routes

--- blogpost-frontend/src/App.jsx
+++ blogpost-frontend/src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
 import LoginComponent from './components/LoginComponent'
 import RegistrationComponent from './components/RegistrationComponent'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+
 function App() {
   return (
     <>
-     <LoginComponent/>
-     <RegistrationComponent/>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login' element= {<LoginComponent/>}> </Route>
+        <Route path='/registration' element = {<RegistrationComponent/>}></Route>
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
--- blogpost-frontend/src/components/RegistrationComponent.jsx
+++ blogpost-frontend/src/components/RegistrationComponent.jsx
@@ -2,7 +2,18 @@
 const RegistrationComponent = () => {
     return (
         <>
-        <form class="max-w-sm mx-auto">
+        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+              <img
+                className="mx-auto h-10 w-auto"
+                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                alt="Your Company"
+              />
+              <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                Sign Up 
+              </h2>
+        </div>
+        <form class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
         <div class="mb-5">
             <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
             <input type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required />
@@ -23,6 +34,7 @@ const RegistrationComponent = () => {
         </div>
         <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register new account</button>
         </form>
+        </div>
         </>
     )
 }
