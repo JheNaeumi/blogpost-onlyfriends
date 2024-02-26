27eaa863729d90a141759ac3bce464293fb0a4a5
Jhe <jeremiahpasion56@gmail.com>
2024-02-26 23:40:00 +0800

    add blogpostComponent

--- blogpost-frontend/src/App.jsx
+++ blogpost-frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import LoginComponent from './components/LoginComponent'
 import RegistrationComponent from './components/RegistrationComponent'
+import BlogpostComponent from './components/BlogpostComponent'
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/login' element= {<LoginComponent/>}> </Route>
         <Route path='/registration' element = {<RegistrationComponent/>}></Route>
+        <Route path='/feed'element ={<BlogpostComponent/>}></Route>
       </Routes>
     </BrowserRouter>
     </>
