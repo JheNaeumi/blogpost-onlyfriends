b47e48ebaf4f8c6d00d20163655a61ade7fdb874
Jhe <jeremiahpasion56@gmail.com>
2024-02-28 23:40:42 +0800

    update profile component

--- blogpost-frontend/src/App.jsx
+++ blogpost-frontend/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import LoginComponent from './components/LoginComponent'
 import RegistrationComponent from './components/RegistrationComponent'
 import BlogpostComponent from './components/BlogpostComponent'
+import ProfileComponent from './components/ProfileComponent'
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path='/login' element= {<LoginComponent/>}> </Route>
         <Route path='/registration' element = {<RegistrationComponent/>}></Route>
         <Route path='/feed'element ={<BlogpostComponent/>}></Route>
+        <Route path='/profile' element ={<ProfileComponent/>}></Route>
       </Routes>
     </BrowserRouter>
     </>
