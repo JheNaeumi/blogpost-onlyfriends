56db3b9bd5cdf61ae13a7716dcd78bd99c0381b6
Jhe <jeremiahpasion56@gmail.com>
2024-02-28 23:20:19 +0800

    add profile component

--- blogpost-frontend/src/components/BlogpostComponent.jsx
+++ blogpost-frontend/src/components/BlogpostComponent.jsx
@@ -30,7 +30,8 @@ const BlogpostComponent = () => {
                 <div class="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
                     <h2 class="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Our Blog</h2>
                     <p class="font-light text-gray-500 sm:text-xl dark:text-gray-400">We use an agile approach to test assumptions and connect with the needs of your audience early and often.</p>
-                </div> 
+                </div>
+               
                 <div class="grid gap-8 lg:grid-cols-2">
                     {people.map((person) =>
                     <article class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
