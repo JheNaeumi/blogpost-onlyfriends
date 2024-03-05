dc90896bc014a41b12a42320fbe69bd1007f8960
Jhe <jeremiahpasion56@gmail.com>
2024-03-05 23:23:51 +0800

    init backend

---
+++ blogpost-backend/src/test/java/com/example/jhenaeumi/BlogpostOnlyfriendsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jhenaeumi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogpostOnlyfriendsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
