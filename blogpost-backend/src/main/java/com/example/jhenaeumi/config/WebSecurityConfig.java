cc39504934efd8ddc1088a939e3dd831e123d0a8
Jhe <jeremiahpasion56@gmail.com>
2024-03-10 23:04:06 +0800

    add websecuirty config

---
+++ blogpost-backend/src/main/java/com/example/jhenaeumi/config/WebSecurityConfig.java
@@ -0,0 +1,52 @@
+package com.example.jhenaeumi.config;
+
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true)
+public class WebSecurityConfig {
+
+    private static final String[] WHITELIST = {
+            "/"
+    };
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers(antMatcher("/css/**")).permitAll();
+                    auth.requestMatchers(antMatcher("/js/**")).permitAll();
+                    auth.requestMatchers(antMatcher("/images/**")).permitAll();
+                    auth.requestMatchers(antMatcher("/fonts/**")).permitAll();
+                    auth.requestMatchers(antMatcher("/webjars/**")).permitAll();
+                    auth.requestMatchers(antMatcher("/")).permitAll();
+                    auth.requestMatchers(antMatcher("/rss/**")).permitAll();
+                    auth.requestMatchers(antMatcher("/register/**")).permitAll();
+                    auth.requestMatchers(antMatcher("/posts/**")).permitAll();
+                    auth.anyRequest().authenticated();
+                })
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .loginProcessingUrl("/login")
+                        .usernameParameter("email")
+                        .passwordParameter("password")
+                        .defaultSuccessUrl("/")
+                        .failureUrl("/login?error")
+                        .permitAll()
+                );
+
+
+        return http.build();
+    }
+
+}
