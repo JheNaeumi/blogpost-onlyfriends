1465d4b72ed211879bac312abd8cde42707f183a
Jhe <jeremiahpasion56@gmail.com>
2024-03-12 23:05:30 +0800

    update App Component

--- blogpost-frontend/src/App.jsx
+++ blogpost-frontend/src/App.jsx
@@ -1,20 +1,36 @@
 import './App.css'
+import { useEffect, useState } from 'react'
+
 import LoginComponent from './components/LoginComponent'
 import RegistrationComponent from './components/RegistrationComponent'
 import BlogpostComponent from './components/BlogpostComponent'
 import ProfileComponent from './components/ProfileComponent'
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import {Navigate, BrowserRouter, Route, Routes} from 'react-router-dom'
+
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    // Check authentication status here (e.g., by verifying the token)
+    const token = localStorage.getItem('authToken');
+    setIsAuthenticated(!!token); // Set authentication status based on token presence
+  }, []);
   return (
     <>
     <BrowserRouter>
-      <Routes>
+      {/* <Routes>
         <Route path='/login' element= {<LoginComponent/>}> </Route>
         <Route path='/registration' element = {<RegistrationComponent/>}></Route>
-        <Route path='/feed'element ={<BlogpostComponent/>}></Route>
+        <Route path='/'element ={<BlogpostComponent/>}></Route>
         <Route path='/profile' element ={<ProfileComponent/>}></Route>
-      </Routes>
+      </Routes> */}
+      <Routes>
+          <Route path="/login"element={<LoginComponent setIsAuthenticated={setIsAuthenticated} />}></Route>
+          <Route path="/registration" element={<RegistrationComponent />} />
+          <Route path="/" element={ isAuthenticated ? ( <BlogpostComponent /> ) : ( <Navigate to="/login" /> ) } />
+          <Route path="/profile" element={ isAuthenticated ? ( <ProfileComponent /> ) : ( <Navigate to="/login" /> ) }/>
+        </Routes>
     </BrowserRouter>
     </>
   )
