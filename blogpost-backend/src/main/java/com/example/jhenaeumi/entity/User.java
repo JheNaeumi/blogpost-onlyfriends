63566587df92f65362301cd08ed11fb8ab5bd065
Jhe <jeremiahpasion56@gmail.com>
2024-03-14 23:54:00 +0800

    update userEntity

---
+++ blogpost-backend/src/main/java/com/example/jhenaeumi/entity/User.java
@@ -0,0 +1,42 @@
+package com.example.jhenaeumi.entity;
+
+import com.example.jhenaeumi.enums.Role;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    @Size(max = 100)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    @Size(max = 100)
+    private String lastName;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Column(nullable = false)
+    @Size(max = 100)
+    private String login;
+
+    @Column(nullable = false)
+    @Size(max = 100)
+    private String password;
+}
