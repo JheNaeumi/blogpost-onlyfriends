16c9a2507200f0373725a7fb288975db17cf0026
Jhe <jeremiahpasion56@gmail.com>
2024-03-20 22:58:41 +0800

    add commentEntity

---
+++ blogpost-backend/src/main/java/com/example/jhenaeumi/entity/Comment.java
@@ -0,0 +1,22 @@
+package com.example.jhenaeumi.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@Table(name = "comments")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String content;
+
+    @ManyToOne
+    private Post post;
+}
