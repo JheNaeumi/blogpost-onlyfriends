fa70c5d0e3f75cd86cdd71b2acf83f5c5b110d83
Jhe <jeremiahpasion56@gmail.com>
2024-03-24 01:05:04 +0800

    update PostRepo

--- blogpost-backend/src/main/java/com/example/jhenaeumi/repository/PostRepo.java
+++ blogpost-backend/src/main/java/com/example/jhenaeumi/repository/PostRepo.java
@@ -1,9 +1,13 @@
 package com.example.jhenaeumi.repository;
 
 import com.example.jhenaeumi.entity.Post;
+import com.example.jhenaeumi.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PostRepo extends JpaRepository<Post, Long> {
+    List<Post> findByUser(User user);
 }
