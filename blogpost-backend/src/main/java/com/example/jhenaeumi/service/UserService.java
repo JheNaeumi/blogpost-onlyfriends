1488cb4fb6b0ed534d3ffe54961bb17cf58a8530
Jhe <jeremiahpasion56@gmail.com>
2024-03-23 20:15:48 +0800

    add UserService interface

--- blogpost-backend/src/main/java/com/example/jhenaeumi/service/UserService.java
+++ blogpost-backend/src/main/java/com/example/jhenaeumi/service/UserService.java
@@ -3,57 +3,11 @@ package com.example.jhenaeumi.service;
 import com.example.jhenaeumi.dto.CredentialsDto;
 import com.example.jhenaeumi.dto.SignUpDto;
 import com.example.jhenaeumi.dto.UserDto;
-import com.example.jhenaeumi.entity.User;
-import com.example.jhenaeumi.enums.Role;
-import com.example.jhenaeumi.exceptions.AppException;
-import com.example.jhenaeumi.mappers.UserMapper;
-import com.example.jhenaeumi.repository.UserRepo;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
 
-import java.nio.CharBuffer;
-import java.util.Optional;
+public interface UserService {
+    UserDto login(CredentialsDto credentialsDto);
 
-@RequiredArgsConstructor
-@Service
-public class UserService {
-    private final UserRepo userRepository;
+    UserDto register(SignUpDto signUpDto);
 
-    private final PasswordEncoder passwordEncoder;
-
-    private final UserMapper userMapper;
-
-    public UserDto login(CredentialsDto credentialsDto) {
-        User user = userRepository.findByLogin(credentialsDto.getLogin())
-                .orElseThrow(() -> new AppException("Unknown user", HttpStatus.NOT_FOUND));
-
-        if (passwordEncoder.matches(CharBuffer.wrap(credentialsDto.getPassword()), user.getPassword())) {
-            return userMapper.toUserDto(user);
-        }
-        throw new AppException("Invalid password", HttpStatus.BAD_REQUEST);
-    }
-
-    public UserDto register(SignUpDto userDto) {
-        Optional<User> optionalUser = userRepository.findByLogin(userDto.getLogin());
-
-        if (optionalUser.isPresent()) {
-            throw new AppException("Login already exists", HttpStatus.BAD_REQUEST);
-        }
-
-        User user = userMapper.signUpToUser(userDto);
-        user.setRole(Role.USER);
-        user.setPassword(passwordEncoder.encode(CharBuffer.wrap(userDto.getPassword())));
-
-        User savedUser = userRepository.save(user);
-
-        return userMapper.toUserDto(savedUser);
-    }
-
-    public UserDto findByLogin(String login) {
-        User user = userRepository.findByLogin(login)
-                .orElseThrow(() -> new AppException("Unknown user", HttpStatus.NOT_FOUND));
-        return userMapper.toUserDto(user);
-    }
+    UserDto findByLogin(String login);
 }
