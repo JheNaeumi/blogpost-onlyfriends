27dbcc62e0c24b27ba23d17615a5bcb9542c2456
Jhe <jeremiahpasion56@gmail.com>
2024-03-25 00:12:28 +0800

    add CommentDto

---
+++ blogpost-backend/src/main/java/com/example/jhenaeumi/dto/CommentDto.java
@@ -0,0 +1,14 @@
+package com.example.jhenaeumi.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class CommentDto {
+    private Long id;
+
+    private String content;
+}
