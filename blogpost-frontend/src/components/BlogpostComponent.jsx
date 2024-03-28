df1fbce55ee25d8895b7279f24401a6ebe0a2ef7
Jhe <jeremiahpasion56@gmail.com>
2024-03-28 23:53:51 +0800

    update blogpostComp

--- blogpost-frontend/src/components/BlogpostComponent.jsx
+++ blogpost-frontend/src/components/BlogpostComponent.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react"
 import { getAuthToken,setAuthHeader } from "../service/AuthService"
 import { useNavigate } from "react-router-dom";
 import jprofile from "../assets/jprofile.png"
+import axios from "axios";
 const BlogpostComponent = () => {
  
   const navigate = useNavigate();
@@ -10,9 +11,12 @@ const BlogpostComponent = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [isFixed, setIsFixed] = useState(false);
   const [isAuth, setisAuth] = useState(false)
+  const [mappedContent, mapContent] = useState([])
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
   useEffect(() => { 
           //TODO: Replace with a service function
-          fetch("http://localhost:8080/api/getAllPost", {
+          fetch(`http://localhost:8080/api/getAllPost`, {
               method: "GET",
               headers: {'Authorization': `Bearer ${getAuthToken()}`}
           }).then(response => {
@@ -20,6 +24,7 @@ const BlogpostComponent = () => {
                   console.log("Successful")
                   setisAuth(true)
                   return response.json()
+                 
               }else{
                 console.log("reponse error")
                   setAuthHeader(null)
@@ -28,12 +33,14 @@ const BlogpostComponent = () => {
               } 
           }).then(data => {
               //TODO: map data into array
+              mapContent(data.content)
+              
           }).catch(()=>{
             setAuthHeader(null)
             navigate('/login')
           })
          
-          
+          fetchData(currentPage);
           const handleResize = () => {
             setIsFixed(window.innerWidth < 768);
             setIsOpen(window.innerWidth >= 768)
@@ -57,100 +64,26 @@ const BlogpostComponent = () => {
         setAuthHeader(null)
         navigate('/login')
       }
+      const handlePageChange = (page) => {
+        setCurrentPage(page);
+      };
+      const fetchData = async (page) => {
+        try {
+          const response = await axios.get(`http://localhost:8080/api/getAllPost?pageNumber=${page}`, {
+            method: "GET",
+            headers: {'Authorization': `Bearer ${getAuthToken()}`}
+        })
+        //setData(response.data.items); // Assuming API returns an array of items
+        setTotalPages(response.data.totalPages);
+        mapContent(response.data.content)
+        console.log("success")
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      };
       if(!isAuth){
         return null;
       }
-        
-  const people = [{
-      name: 'Leslie Alexander',
-      email: 'leslie.alexander@example.com',
-      role: 'Co-Founder / CEO',
-      caption:'Hello World',
-      post: 'Static websites are now used to bootstrap lots of websites and are becoming the basis for a variety of tools that even influence both web designers and developers influence both web designers and developers',
-      imageUrl:
-        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      lastSeen: '3h ago',
-      lastSeenDateTime: '2023-01-23T13:23Z',
-    },
-    {
-      name: 'Leslie Alexander 4th',
-      email: 'leslie.alexander@example.com',
-      role: 'Co-Founder / CEO',
-      caption:'Hello World',
-      post: 'Static websites are now used to bootstrap lots of websites and are becoming the basis for a variety of tools that even influence both web designers and developers influence both web designers and developers',
-      imageUrl:
-        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      lastSeen: '3h ago',
-      lastSeenDateTime: '2023-01-23T13:23Z',
-    },
-    {
-      name: 'Leslie Alexander 2nd',
-      email: 'leslie.alexander@example.com',
-      role: 'Co-Founder / CEO',
-      caption:'Hello World',
-      post: 'Static websites are now used to bootstrap lots of websites and are becoming the basis for a variety of tools that even influence both web designers and developers influence both web designers and developers',
-      imageUrl:
-        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      lastSeen: '3h ago',
-      lastSeenDateTime: '2023-01-23T13:23Z',
-    },
-    {
-      name: 'Leslie Alexander 3rd',
-      email: 'leslie.alexander@example.com',
-      role: 'Co-Founder / CEO',
-      caption:'Hello World',
-      post: 'Static websites are now used to bootstrap lots of websites and are becoming the basis for a variety of tools that even influence both web designers and developers influence both web designers and developers',
-      imageUrl:
-        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      lastSeen: '3h ago',
-      lastSeenDateTime: '2023-01-23T13:23Z',
-    },
-    {
-      name: 'Leslie Alexander 8 th',
-      email: 'leslie.alexander@example.com',
-      role: 'Co-Founder / CEO',
-      caption:'Hello World',
-      post: 'Static websites are now used to bootstrap lots of websites and are becoming the basis for a variety of tools that even influence both web designers and developers influence both web designers and developers',
-      imageUrl:
-        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      lastSeen: '3h ago',
-      lastSeenDateTime: '2023-01-23T13:23Z',
-    },
-    {
-      name: 'Leslie Alexander 5th',
-      email: 'leslie.alexander@example.com',
-      role: 'Co-Founder / CEO',
-      caption:'Hello World',
-      post: 'Static websites are now used to bootstrap lots of websites and are becoming the basis for a variety of tools that even influence both web designers and developers influence both web designers and developers',
-      imageUrl:
-        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      lastSeen: '3h ago',
-      lastSeenDateTime: '2023-01-23T13:23Z',
-    },
-    {
-      name: 'Leslie Alexander 6th',
-      email: 'leslie.alexander@example.com',
-      role: 'Co-Founder / CEO',
-      caption:'Hello World',
-      post: 'Static websites are now used to bootstrap lots of websites and are becoming the basis for a variety of tools that even influence both web designers and developers influence both web designers and developers',
-      imageUrl:
-        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      lastSeen: '3h ago',
-      lastSeenDateTime: '2023-01-23T13:23Z',
-    },
-    {
-      name: 'Leslie Alexander 7th',
-      email: 'leslie.alexander@example.com',
-      role: 'Co-Founder / CEO',
-      caption:'Hello World',
-      post: 'Static websites are now used to bootstrap lots of websites and are becoming the basis for a variety of tools that even influence both web designers and developers influence both web designers and developers',
-      imageUrl:
-        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      lastSeen: '3h ago',
-      lastSeenDateTime: '2023-01-23T13:23Z',
-    },
-  ]
-  
   
   return (
     <>  
@@ -202,6 +135,7 @@ const BlogpostComponent = () => {
           </div> */}
         </div>
         {/* Main content */}
+     
         <div className="flex flex-col flex-1">
          
           <div className={` h-full overflow-y-auto bg-slate-50 dark:bg-gray-900 ${isOpen&&isFixed? 'opacity-50': ''}`}>
@@ -216,33 +150,86 @@ const BlogpostComponent = () => {
                   <p className="font-light text-gray-500 sm:text-xl dark:text-gray-400">We use an agile approach to test assumptions and connect with the needs of your audience early and often.</p>
               </div>
               <div className="grid gap-8 lg:grid-cols-2">
-                {people.map((person) =>
-                <article key={person.name}className="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+                {mappedContent.map((post) =>
+                <article key={post.id}className="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
                     <div className="flex justify-between items-center mb-5 text-gray-500">
                         <span className="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
-                            <svg className="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path></svg>
+                            <svg className="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path>
+                           
+                            </svg>
                         </span>
-                        <span className="text-sm">{person.lastSeen}</span>
+                        <span className="text-sm">{new Date(post.postCreatedDate).toLocaleString()}</span>
                     </div>
-                    <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><a href="#">{person.caption}</a></h2>
-                    <p className="mb-5 font-light text-gray-500 dark:text-gray-400">{person.post}</p>
+                    <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><a href="#">{post.postTitle}</a></h2>
+                    <p className="mb-5 font-light text-gray-500 dark:text-gray-400">{post.postContent}</p>
                     <div className="flex flex-wrap justify-between items-center">
                         <div className="flex items-center space-x-4">
                             <img className="w-7 h-7 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png" alt="Jese Leos avatar" />
                             <span className="font-medium dark:text-white">
-                                {person.name}
+                              {post.user.firstName} {post.user.lastName}
                             </span>
                         </div>
                         <span className="flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline">
-                            {person.role}
+                        {post.category.categoryTitle}
                         </span>
                     </div>
                 </article>
-                )}              
+                )}
+                         
               </div>  
             </div> 
           </div>
           
+          <div>
+            <button disabled={currentPage === 1} onClick={() => handlePageChange(currentPage - 1)}>
+              Previous
+            </button>
+            <span>{currentPage}</span>
+            <button disabled={currentPage === totalPages} onClick={() => handlePageChange(currentPage + 1)}>
+              Next
+            </button>
+          </div>
+          {/* <nav aria-label="Page navigation example">
+            <ul class="list-style-none flex">
+              <li>
+                <button
+                  class="relative block rounded bg-transparent px-3 py-1.5 text-sm text-surface transition duration-300 hover:bg-neutral-100 focus:bg-neutral-100 focus:text-primary-700 focus:outline-none active:bg-neutral-100 active:text-primary-700 dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700 dark:focus:text-primary-500 dark:active:bg-neutral-700 dark:active:text-primary-500"
+                
+                  aria-label="Previous" disabled={currentPage === 1} onClick={() => handlePageChange(currentPage - 1)}>
+                  <span aria-hidden="true">&laquo;</span>
+                </button>
+              </li>
+              <li>
+                <a
+                  class="relative block rounded bg-transparent px-3 py-1.5 text-sm text-surface transition duration-300 hover:bg-neutral-100 focus:bg-neutral-100 focus:text-primary-700 focus:outline-none active:bg-neutral-100 active:text-primary-700 dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700 dark:focus:text-primary-500 dark:active:bg-neutral-700 dark:active:text-primary-500"
+                  href="#"
+                  >1</a
+                >
+              </li>
+              <li aria-current="page">
+                <a
+                  class="relative block rounded bg-transparent px-3 py-1.5 text-sm text-surface transition duration-300 hover:bg-neutral-100 focus:bg-neutral-100 focus:text-primary-700 focus:outline-none active:bg-neutral-100 active:text-primary-700 dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700 dark:focus:text-primary-500 dark:active:bg-neutral-700 dark:active:text-primary-500"
+                  href="#"
+                  >2</a
+                >
+              </li>
+              <li>
+                <a
+                  class="relative block rounded bg-transparent px-3 py-1.5 text-sm text-surface transition duration-300 hover:bg-neutral-100 focus:bg-neutral-100 focus:text-primary-700 focus:outline-none active:bg-neutral-100 active:text-primary-700 dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700 dark:focus:text-primary-500 dark:active:bg-neutral-700 dark:active:text-primary-500"
+                  href="#"
+                  >3</a
+                >
+              </li>
+              <li>
+                <button
+                  class="relative block rounded bg-transparent px-3 py-1.5 text-sm text-surface transition duration-300 hover:bg-neutral-100 focus:bg-neutral-100 focus:text-primary-700 focus:outline-none active:bg-neutral-100 active:text-primary-700 dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700 dark:focus:text-primary-500 dark:active:bg-neutral-700 dark:active:text-primary-500"
+                 
+                  aria-label="Next"  disabled={currentPage === totalPages} onClick={() => handlePageChange(currentPage + 1)}
+                  ><span aria-hidden="true">&raquo;</span>
+                </button>
+              </li>
+            </ul>
+          </nav> */}
         </div>
       </div>
   
