722108671b48c51a0fce741397cfb8c8bfa538df
Jhe <jeremiahpasion56@gmail.com>
2024-03-29 23:13:04 +0800

    update loginService

--- blogpost-frontend/src/service/LoginService.js
+++ blogpost-frontend/src/service/LoginService.js
@@ -6,8 +6,6 @@ export const postLogin = (userLogin) => {
      return axios.post(REST_API_BASE_URL + "/login", userLogin)
 }
 
-
-// dummy service for message
-export const getMessage = () => {
-    return axios.get(REST_API_BASE_URL + "/messages")
+export const updateProfile = () => {
+     
 }
