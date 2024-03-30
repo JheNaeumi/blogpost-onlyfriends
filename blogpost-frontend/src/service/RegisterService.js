0fea0ca31c5d379689eb5a4e58c7a75004e65756
Jhe <jeremiahpasion56@gmail.com>
2024-03-30 22:00:45 +0800

    update RegisterService

--- blogpost-frontend/src/service/RegisterService.js
+++ blogpost-frontend/src/service/RegisterService.js
@@ -6,3 +6,11 @@ const REST_API_BASE_URL ='http://localhost:8080';
 export const postRegistration = (registration) => {
     return axios.post(REST_API_BASE_URL +"/register", registration)
 }
+
+export const updateProfile = (userDetails) => {
+    return axios.put(REST_API_BASE_URL+"/profile", userDetails)
+}
+
+export const getProfile = (token) => {
+    axios.get(REST_API_BASE_URL+"/getProfile", token)
+}
