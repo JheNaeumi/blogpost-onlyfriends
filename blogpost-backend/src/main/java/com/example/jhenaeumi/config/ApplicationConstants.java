b65198fbed9d25aed349a608f09acd06cd7b62c7
Jhe <jeremiahpasion56@gmail.com>
2024-03-31 23:49:03 +0800

    update paths and var

--- blogpost-backend/src/main/java/com/example/jhenaeumi/config/ApplicationConstants.java
+++ blogpost-backend/src/main/java/com/example/jhenaeumi/config/ApplicationConstants.java
@@ -3,7 +3,7 @@ package com.example.jhenaeumi.config;
 public class ApplicationConstants {
 
     public static final String PAGE_NUMBER ="0";
-    public static final String PAGE_SIZE ="5";
+    public static final String PAGE_SIZE ="4";
     public static final String SORT_BY ="id";
 
     public static final Integer ADMIN_USER_ID_HARDCODED_ID_VALUE =7001;//I have inserted '7002' for ADMIN ROLE ID manually in db
