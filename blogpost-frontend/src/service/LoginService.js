9722faf05ec176465faa292edac5bf9601719a30
Jhe <jeremiahpasion56@gmail.com>
2024-04-07 20:34:52 +0800

    old service

--- blogpost-frontend/src/service/LoginService.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const REST_API_BASE_URL ='http://localhost:8080';
-
-export const postLogin = (userLogin) => {
-     return axios.post(REST_API_BASE_URL + "/login", userLogin)
-}
-
-
