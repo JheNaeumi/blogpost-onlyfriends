83a08d73c3a751a618fff1be2bab53927e7b622e
Jhe <jeremiahpasion56@gmail.com>
2024-04-12 22:41:10 +0800

    add CommentService

--- blogpost-frontend/src/service/CommentService.js
+++ blogpost-frontend/src/service/CommentService.js
@@ -2,6 +2,18 @@ import axios from "axios"
 
 const REST_API_BASE_URL ='http://localhost:8080/api/comment';
 
-export const postComment = () => {
-    return axios.post(REST_API_BASE_URL+`/create`)
+export const postComment = (token, comment, postId) => {
+    return axios.post(REST_API_BASE_URL+`/create/${postId}`, comment, {
+        headers: {'Authorization': `Bearer ${token}`}
+    })
+}
+export const updateComment = (token, commentId, comment ) => {
+    return axios.patch(REST_API_BASE_URL+`/update/${commentId}`, comment, {
+        headers: {'Authorization': `Bearer ${token}`}
+    })
+}
+export const deleteComment = (token, commentId) => {
+    axios.delete(REST_API_BASE_URL+`/delete/${commentId}`, {
+        headers: {'Authorization': `Bearer ${token}`}
+    })
 }
