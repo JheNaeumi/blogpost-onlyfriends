17793d4537916230720455642aeaf8268bf0475b
Jhe <jeremiahpasion56@gmail.com>
2024-04-12 22:41:56 +0800

    add delete & update Content

--- blogpost-frontend/src/service/PostService.js
+++ blogpost-frontend/src/service/PostService.js
@@ -13,7 +13,18 @@ export const getPostResponse = (token, page) => {
          headers: {'Authorization': `Bearer ${token}`}})
  }
  
- export const postUserContent = (token, userId, content) => {
-     return axios.post(REST_API_BASE_URL+`/user/${userId}/category/1`, content,{
+ export const postUserContent = (token, content) => {
+     return axios.post(REST_API_BASE_URL+`/user/category/1`, content,{
          headers: {'Authorization': `Bearer ${token}`}})
+ }
+
+ export const deleteUserContent =(token, postId) => {
+    axios.delete(REST_API_BASE_URL + `/user/delete/${postId}`, {
+        headers: {'Authorization' : `Bearer ${token}`}
+    })
+ }
+ export const updateUserContent = (token, postId, content) => {
+    return axios.patch(REST_API_BASE_URL + `/user/update/${postId}`,content, {
+        headers: {'Authorization': `Bearer ${token}`}
+    } )
  }
