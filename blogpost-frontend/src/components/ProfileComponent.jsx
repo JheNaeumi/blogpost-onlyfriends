33726db38a0f6111eb81253e9c6cbd8ef37e6d98
Jhe <jeremiahpasion56@gmail.com>
2024-04-20 14:10:04 +0800

    refractor & add comments

--- blogpost-frontend/src/components/ProfileComponent.jsx
+++ blogpost-frontend/src/components/ProfileComponent.jsx
@@ -4,73 +4,67 @@ import { getAuthToken } from "../service/AuthService";
 
 
 const ProfileComponent = () => {
-    const token = getAuthToken();
-    const [formData, setFormData] = useState({
-      firstName: '',
-      lastName: '',
-      login: '',
-      password: '',
-      
-    },[]);
-    const [formData_1, setFormData_1] = useState({
-      repeatPassword: ''
-    },[])
-    const [passwordError, setPasswordError] = useState('');
-    
-    useEffect(() =>{
-      getUserProfileData();
-
-    },[])
-    
-    const getUserProfileData = async() =>  {
-      try {
-        const response = await getProfile(token)
-        if(response.status === 200){
-          setFormData(response.data)
-      }
-      } catch (error) {
-        console.log(error)
-      }
-    }
-
-      const handleSubmit = async (e) => {
-        e.preventDefault();
-    
-      try {
-        if (formData.password !== formData_1.repeatPassword) {
-            setPasswordError('Passwords do not match');
-            return;
-        }
-        else{
-          
-            const response = await updateProfile(token, formData)
-            if (response.status === 200) {
-              console.log('Update Succesful',response.data);
-            }else {
-              console.error('Update failed', response.statusText);
-              setFormData('')
-              setFormData_1('')
-            }
+  const token = getAuthToken();
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    login: '',
+    password: '',
+  },[]);
+  const [formData_1, setFormData_1] = useState({
+    repeatPassword: ''
+  },[])
+  const [passwordError, setPasswordError] = useState('');
   
+  useEffect(() =>{
+    getUserProfileData();
+  },[])
+  
+  const getUserProfileData = async() =>  {
+    try {
+      const response = await getProfile(token)
+      if(response.status === 200){
+        setFormData(response.data)
+    }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      if (formData.password !== formData_1.repeatPassword) {
+          setPasswordError('Passwords do not match');
+          return;
+      }
+      else{
+          const response = await updateProfile(token, formData)
+          if (response.status === 200) {
+            console.log('Update Succesful',response.data);
+          }else {
+            console.error('Update failed', response.statusText);
+            setFormData('')
+            setFormData_1('')
           }
-          
-      } catch (error) {
-        // Handle registration error (e.g., show an error message)
-        console.error('Registration failed', error);
-      }
-      }
-    return (
-        <>
-        <div className="h-full mx-auto px-6 py-12 lg:px-8 ">
-          <div className="border-b-2 block md:flex rounded-lg">
-            <div className="w-full md:w-2/5 p-4 sm:p-6 lg:p-8 bg-white shadow-md">
-              <span className="text-xl font-semibold block">User Profile</span>
-              <span className="text-gray-600">This information is sensitive</span>
-              <div className="w-full p-8 mx-2 flex justify-center">
-                <img width="256" height="256" src="https://img.icons8.com/windows/256/user.png" alt="user"/>
-              </div>
-              <span className="text-xl font-semibold flex justify-center">{formData.login}</span>
+        }
+    } catch (error) {
+      console.error('Registration failed', error);
+    }
+  }
+  return (
+    <>
+      <div className="h-full mx-auto px-6 py-12 lg:px-8 ">
+        <div className="border-b-2 block md:flex rounded-lg">
+          <div className="w-full md:w-2/5 p-4 sm:p-6 lg:p-8 bg-white shadow-md">
+            <span className="text-xl font-semibold block">User Profile</span>
+            <span className="text-gray-600">This information is sensitive</span>
+            <div className="w-full p-8 mx-2 flex justify-center">
+              <img width="256" height="256" src="https://img.icons8.com/windows/256/user.png" alt="user"/>
             </div>
+            <span className="text-xl font-semibold flex justify-center">{formData.login}</span>
+          </div>
+          {/* Form for Updating User Profile */}
           <form className="mt-8 sm:mx-auto sm:w-full sm:max-w-sm py-3" onSubmit={handleSubmit}>
             <div className="mb-5">
                 <label htmlFor="firstName" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">FirstName</label>
@@ -82,11 +76,6 @@ const ProfileComponent = () => {
                 <input type="lastName" id="lastName" name="lastName" 
                 className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"  required value={formData.lastName || ''} onChange={(e) => setFormData({...formData, lastName:e.target.value})} />
             </div>
-            {/* <div className="mb-5">
-                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                <input type="email" id="" name="login" disabled
-                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required value={formData.login || ''} onChange={(e) => setFormData({ login:e.target.value})} />
-            </div> */}
             <div className="mb-5">
                 <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
                 <input type="password" id="password" name="password" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required value={formData.password || ''} onChange={(e) => setFormData({...formData, password:e.target.value})}/>
@@ -103,10 +92,10 @@ const ProfileComponent = () => {
             <a href="/" className="text-white bg-red-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Cancel</a>
             </div>
           </form>
-          </div>
         </div>
-        </>
-    )
+      </div>
+    </>
+  )
 }
 
 export default ProfileComponent
