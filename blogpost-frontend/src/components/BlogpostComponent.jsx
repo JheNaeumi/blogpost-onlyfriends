7f47210a834b505adb24675aa17e8fdaeec45fe3
Jhe <jeremiahpasion56@gmail.com>
2024-04-20 13:27:04 +0800

    refractor & add comments

--- blogpost-frontend/src/components/BlogpostComponent.jsx
+++ blogpost-frontend/src/components/BlogpostComponent.jsx
@@ -110,7 +110,7 @@ const BlogpostComponent = () => {
     <>  
       <div className="flex h-screen relative">
         <div className={`h-full bg-slate-300 drop-shadow-xl border-r rounded-lg dark:bg-gray-800 dark:border-gray-600 transition-all duration-600 ease-in-out  ${isOpen ? 'block' :'hidden' } ${isFixed ? 'fixed top-0 left-0  w-64' : 'flex flex-col w-64'} ${isFixed ? 'z-50' : ''}`}>
-           {/*TODO: Add an exit button for the sidebar*/}
+          {/* SideBar Navigation */}
           <div className={`${isFixed? 'block':'hidden'}`}>
             <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 absolute top-2.5 end-2.5 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"onClick={toggleSidebar} >
               <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
@@ -120,75 +120,64 @@ const BlogpostComponent = () => {
           <div className="flex flex-col items-center justify-center py-14 dark:border-gray-600">
             <img width="96" height="96" src="https://img.icons8.com/windows/96/user.png" alt="user"/>
             <span className="text-xl font-semibold flex justify-center">{formData.firstName} {formData.lastName}</span>
-          </div>
-          
+          </div>      
           <div className="overflow-y-auto">
             <ul className="space-y-8 font-medium">
-            <li>
+              <li>
                 <a href="" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                   <svg className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
-                      <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z"/>
-                      <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z"/>
+                    <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z"/>
+                    <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z"/>
                   </svg>
                   <span className="ms-3">Dashboard</span>
                 </a>
-            </li>  
-            <li>
+              </li>  
+              <li>
                 <a href="/profile" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                   <svg className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
                       <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z"/>
                   </svg>
                   <span className="flex-1 ms-3 whitespace-nowrap">Profile</span>
                 </a>
-            </li>
-            <li>
+              </li>
+              <li>
                 <a href="" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"onClick={logOut}>
                   <svg className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 16" >
                       <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3"/>
                   </svg>
                   <span className="flex-1 ms-3 whitespace-nowrap">Sign Out</span>
                 </a>
-            </li>
+              </li>
             </ul>
-            {/* Add more sidebar links as needed */}
           </div>
-          {/* <div className="flex items-center justify-center h-20 border-t dark:border-gray-600">
-          </div> */}
         </div>
-        {/* Main content */}
-     
+        {/* Main Content */}
         <div className="flex flex-col flex-1">
-         
           <div className={` h-full overflow-y-auto bg-slate-50 dark:bg-gray-900 ${isOpen&&isFixed? 'opacity-50': ''}`}>
             <div className="py-2 px-4 mx-auto max-w-screen lg:py-4 lg:px-6"> 
               <button onClick={toggleSidebar} className="block md:hidden py-2">
                 <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                   <path clipRule="evenodd" fillRule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
                 </svg>
-              </button>    
-              {/* <div className="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8 ">
-                  <h2 className="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Our Blog</h2>
-                  <p className="font-light text-gray-500 sm:text-xl dark:text-gray-400">We use an agile approach to test assumptions and connect with the needs of your audience early and often.</p>
-              </div> */}
+              </button>
+               {/* Create New Post */}    
               <div className="heading text-center font-bold text-2xl m-5 text-gray-800">New Post</div>
               <div className="editor mx-auto w-10/12 flex flex-col text-gray-800 border border-gray-300 p-4 shadow-lg max-w-2xl rounded-lg">
                 <input className="title bg-gray-100 border border-gray-300 p-2 mb-4 outline-none" spellCheck="false" placeholder="Title" type="text" id="postTitle" name="postTitle" required value={content.postTitle || ''} onChange={(e) => setContent({...content, postTitle:e.target.value})}/>
                 <textarea className="description bg-gray-100 sec p-3 h-40 border border-gray-300 outline-none" spellCheck="false" placeholder="Describe everything about this post here"name="postContent" id="postContent" required value={content.postContent || ''} onChange={(e)=> setContent({...content, postContent:e.target.value})}></textarea>
-                
-              
                 <div className="icons flex text-gray-500 m-2">
                   <div className="count ml-auto text-gray-400 text-xs font-semibold">0/300</div>
                 </div>
-              
                 <div className="buttons flex">
                   <button className="btn border border-indigo-500 p-1 px-4 font-semibold cursor-pointer text-gray-200 ml-2 bg-indigo-500" onClick={handlePostUserContent}>Post</button>
                 </div>
               </div>
+               {/* List of Post */}
               <div className="flex justify-center">
                 <div className="grid gap-8 grid-cols-1 pt-12 px-2 w-2/3 ">
-                {mappedContent.map((post) => (
-                  <Post key={post.id} post={post} getPostContent={getPostContent} formData={formData}/>
-                ))}
+                  {mappedContent.map((post) => (
+                    <Post key={post.id} post={post} getPostContent={getPostContent} formData={formData}/>
+                  ))}
                 </div>
               </div>  
             </div> 
@@ -204,8 +193,6 @@ const BlogpostComponent = () => {
         </div>
       </div>
     </>
-    
-      
   )
 }
 
