f7912702c59fa4d99c6d8db864de5eb088ef7e26
Jhe <jeremiahpasion56@gmail.com>
2024-04-27 22:24:14 +0800

    update RateLimited Annotation

--- blogpost-backend/src/main/java/com/example/jhenaeumi/annotation/RateLimited.java
+++ blogpost-backend/src/main/java/com/example/jhenaeumi/annotation/RateLimited.java
@@ -8,6 +8,10 @@ import java.lang.annotation.Target;
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface RateLimited {
-    String value() default ""; // This can be used to identify different rate limit configurations
+    String name() default ""; // This can be used to identify different rate limit configurations
     String token() default ""; // Parameter for passing the token
+
+    int value() default 2;
+
+    long minutes() default 1;
 }
