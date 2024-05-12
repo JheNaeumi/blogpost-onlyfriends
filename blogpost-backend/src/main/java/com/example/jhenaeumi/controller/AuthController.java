d903e1b5d59180750d110a438272a1e2f73fa70c
Jhe <jeremiahpasion56@gmail.com>
2024-05-12 21:02:20 +0800

    add verifyUser

--- blogpost-backend/src/main/java/com/example/jhenaeumi/controller/AuthController.java
+++ blogpost-backend/src/main/java/com/example/jhenaeumi/controller/AuthController.java
@@ -7,9 +7,11 @@ import com.example.jhenaeumi.dto.UserDto;
 import com.example.jhenaeumi.service.impl.UserServiceImpl;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -31,6 +33,14 @@ public class AuthController {
         return ResponseEntity.ok(createdUser);
     }
 
-
+    @PostMapping("/api/verify")
+    public ResponseEntity<?> verifyUser(@RequestParam String email, @RequestParam String otp){
+        try {
+            userService.verify(email, otp);
+            return new ResponseEntity<>("User verified successfully", HttpStatus.OK);
+        }catch (RuntimeException e){
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.BAD_REQUEST);
+        }
+    }
 
 }
