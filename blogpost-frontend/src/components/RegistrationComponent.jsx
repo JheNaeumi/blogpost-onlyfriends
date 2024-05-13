d0919d7c1c4748742f80b8dc0f738552e7b58e70
Jhe <jeremiahpasion56@gmail.com>
2024-05-13 23:22:19 +0800

    update RegistrationComponent

--- blogpost-frontend/src/components/RegistrationComponent.jsx
+++ blogpost-frontend/src/components/RegistrationComponent.jsx
@@ -32,16 +32,28 @@ const RegistrationComponent = () => {
         const response = await postRegistration(formData)
         if (response.status === 200) {
           const data = await response;
-          setAuthHeader(response.data.token);
-          //console.log('Login successful', data);
-          navigate("/")
+          setShowNotification(true)
+          setnotifMessage('Registration Success, Check Email For Verification')
+          setFormData('')
+          setFormData_1({
+            repeatPassword: '', 
+            agreeTerms: false,  
+        });
+          setTimeout(() => {
+            setShowNotification(false);
+            setnotifMessage('')
+          }, 3000);
         }else {
+       
           setAuthHeader(null);
           //console.error('Login failed', response.statusText);
           setShowNotification(true)
           setnotifMessage('Registration Failed, Existing User')
           setFormData('')
-          setFormData_1('')
+          setFormData_1({
+            repeatPassword: '', 
+            agreeTerms: false, 
+        });
           setTimeout(() => {
             setShowNotification(false);
             setnotifMessage('')
@@ -53,13 +65,16 @@ const RegistrationComponent = () => {
       // Handle registration error (e.g., show an error message)
       console.error('Registration failed', error);
       setShowNotification(true)
-      setnotifMessage('Registration Failed, Existing User')
+      setnotifMessage('Registration Failed, Try again later')
       setTimeout(() => {
         setShowNotification(false);
         setnotifMessage('')
       }, 2000);
       setFormData('')
-      setFormData_1('')
+      setFormData_1({
+        repeatPassword: '',
+        agreeTerms: false, 
+    });
     }
   }
   useEffect(()=>{
@@ -114,7 +129,7 @@ const RegistrationComponent = () => {
           )}
           <div className="flex items-start mb-5">
               <div className="flex items-center h-5">
-              <input id="terms" type="checkbox" value="" name="agreeTerms" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required checked={formData_1.agreeTerms} onChange={(e) => setFormData_1({...formData_1, agreeTerms:e.target.checked})}/>
+              <input id="terms" type="checkbox" value="" name="agreeTerms" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required checked={formData_1.agreeTerms } onChange={(e) => setFormData_1({...formData_1, agreeTerms:e.target.checked})}/>
               </div>
               <label htmlFor="terms" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">I agree with the <a href="#" className="text-blue-600 hover:underline dark:text-blue-500">terms and conditions</a></label>
           </div>
