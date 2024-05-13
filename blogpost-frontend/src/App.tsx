4db912f66a37320b9d8f14bbd043fa042baa612c
Jhe <jeremiahpasion56@gmail.com>
2024-05-13 23:21:00 +0800

    add Verification Component

--- blogpost-frontend/src/App.tsx
+++ blogpost-frontend/src/App.tsx
@@ -13,17 +13,19 @@ import React from 'react'
 import AdminComponent from './components/AdminComponent'
 
 import { getAuthToken } from './service/AuthService'
+import VerificationComponent from './components/VerificationComponent'
 
 function App() {
-  
+ 
   return (
     <>
     <BrowserRouter>
       <Routes>
           <Route path='/login' element={<LoginComponent/> }></Route>
           <Route path='/registration' element={<RegistrationComponent/> }></Route>
-            <Route path='/profile' element={getAuthToken!==null&&<ProfileComponent/> }></Route>
-            <Route path='/' element={getAuthToken!==null&&<BlogpostComponent/> }></Route>
+          <Route path='/verify' element={<VerificationComponent/> }></Route>
+            <Route path={'/profile'} element={getAuthToken!==null&&<ProfileComponent/> }></Route>
+            <Route path={'/'} element={getAuthToken!==null&&<BlogpostComponent/> }></Route>
             <Route path='/admin' element={getAuthToken!==null&&<AdminComponent/>}></Route>
         </Routes>
     </BrowserRouter>
