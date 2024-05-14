e815ab4d68cc1f187663f2ead266570db6367d75
Jhe <jeremiahpasion56@gmail.com>
2024-05-14 23:45:35 +0800

    add getAllCategory

--- blogpost-backend/src/main/java/com/example/jhenaeumi/repository/CategoryRepo.java
+++ blogpost-backend/src/main/java/com/example/jhenaeumi/repository/CategoryRepo.java
@@ -3,8 +3,11 @@ package com.example.jhenaeumi.repository;
 import com.example.jhenaeumi.entity.Category;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CategoryRepo extends JpaRepository<Category, Long> {
+    @Query(value = "SELECT * FROM categories WHERE id IS NOT NULL", nativeQuery = true)
+    Category getAllCategory();
 }
