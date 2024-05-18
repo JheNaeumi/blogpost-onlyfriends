4ffa14bf26bcf64493a3de035c33bf8426278f01
Jhe <jeremiahpasion56@gmail.com>
2024-05-18 23:41:04 +0800

    update Category

--- blogpost-backend/src/main/java/com/example/jhenaeumi/entity/Category.java
+++ blogpost-backend/src/main/java/com/example/jhenaeumi/entity/Category.java
@@ -1,6 +1,7 @@
 package com.example.jhenaeumi.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,10 +9,11 @@ import lombok.Setter;
 import java.util.ArrayList;
 import java.util.List;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Setter
 @Getter
-@NoArgsConstructor
 @Table(name="categories")
 public class Category {
     @Id
