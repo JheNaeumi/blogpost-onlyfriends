dbdd93019abeb9ec9968a48c09ea528f21bcfc7c
Jhe <jeremiahpasion56@gmail.com>
2024-05-20 21:53:00 +0800

    update authservice

--- blogpost-frontend/src/service/AuthService.tsx
+++ blogpost-frontend/src/service/AuthService.tsx
@@ -1,8 +1,3 @@
-import axios from "axios";
-
-
-axios.defaults.baseURL = 'http://localhost:8080';
-axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 
 export const getAuthToken = () => {
@@ -15,17 +10,4 @@ export const setAuthHeader = (token: string | null) => {
     } else {
       window.localStorage.removeItem("auth_token");
     }
-};
-export const request = (method, url, data) => {
-
-  let headers = {};
-  if (getAuthToken() !== null && getAuthToken() !== "null") {
-      headers = {'Authorization': `Bearer ${getAuthToken()}`};
-  }
-
-  return axios({
-      method: method,
-      url: url,
-      headers: headers,
-      data: data});
 };
