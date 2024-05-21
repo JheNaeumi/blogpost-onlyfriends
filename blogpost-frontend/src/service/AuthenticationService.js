80747c079ff76c4877c77ead3c1739f2d83bd6be
Jhe <jeremiahpasion56@gmail.com>
2024-05-21 23:35:31 +0800

    add validtoken AuthService

--- blogpost-frontend/src/service/AuthenticationService.js
+++ blogpost-frontend/src/service/AuthenticationService.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-const REST_API_BASE_URL ='http://localhost:8080/api';
 
+const REST_API_BASE_URL ='http://localhost:8080/api'; // dev
+//const REST_API_BASE_URL ='https://onlyfriendsblogpost.online/api'; // production
 
 export const postRegistration = (registration) => {
     return axios.post(REST_API_BASE_URL +"/register", registration)
@@ -12,6 +13,13 @@ export const postLogin = (userLogin) => {
 }
 
 export const verify = (email, otp) => {
-    return axios.post(REST_API_BASE_URL + `/verify?email=${email}&otp=${otp}`)
+    return axios.get(REST_API_BASE_URL + `/verify?email=${email}&otp=${otp}`)
     
 }
+
+export const validToken = (token) => {
+    return axios.get(REST_API_BASE_URL + "/token", {
+        headers: {'Authorization': `Bearer ${token}`}
+    })
+}
+
