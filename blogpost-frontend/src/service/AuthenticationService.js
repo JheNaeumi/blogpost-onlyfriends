bd3e6bd76d0d8f8bb98b98e4640daf55cdef37c5
Jhe <jeremiahpasion56@gmail.com>
2024-05-21 23:36:32 +0800

    remove prod link

--- blogpost-frontend/src/service/AuthenticationService.js
+++ blogpost-frontend/src/service/AuthenticationService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const REST_API_BASE_URL ='http://localhost:8080/api'; // dev
-//const REST_API_BASE_URL ='https://onlyfriendsblogpost.online/api'; // production
+
 
 export const postRegistration = (registration) => {
     return axios.post(REST_API_BASE_URL +"/register", registration)
