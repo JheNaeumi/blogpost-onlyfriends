8eaec415ba9d112bd649ea45914bc53d0c278be8
Jhe <jeremiahpasion56@gmail.com>
2024-05-23 23:40:21 +0800

    update AuthFilter

--- blogpost-backend/src/main/java/com/example/jhenaeumi/config/JwtAuthFilter.java
+++ blogpost-backend/src/main/java/com/example/jhenaeumi/config/JwtAuthFilter.java
@@ -1,13 +1,16 @@
 package com.example.jhenaeumi.config;
 
+import com.example.jhenaeumi.exceptions.AppException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
+
 
 import java.io.IOException;
 
@@ -20,21 +23,28 @@ public class JwtAuthFilter extends OncePerRequestFilter {
             HttpServletRequest httpServletRequest,
             HttpServletResponse httpServletResponse,
             FilterChain filterChain) throws ServletException, IOException {
-        String header = httpServletRequest.getHeader(HttpHeaders.AUTHORIZATION);
-
-        if (header != null) {
-            String[] authElements = header.split(" ");
-
-            if (authElements.length == 2
-                    && "Bearer".equals(authElements[0])) {
-                try {
-                    SecurityContextHolder.getContext().setAuthentication(
-                            userAuthenticationProvider.validateToken(authElements[1]));
-                } catch (RuntimeException e) {
-                    SecurityContextHolder.clearContext();
-                    throw e;
+        String header = null;
+        if(httpServletRequest.getCookies() != null){
+            for(Cookie cookie: httpServletRequest.getCookies()){
+                if(cookie.getName().equals("accessToken")){
+                    header = cookie.getValue();
                 }
             }
+        }
+        //TODO: Change Logic Using Cookies
+        if (header != null) {
+            //String[] authElements = header.split(" ");
+            try {
+                SecurityContextHolder.getContext().setAuthentication(
+                        userAuthenticationProvider.validateToken(header));
+            } catch (RuntimeException e) {
+                SecurityContextHolder.clearContext();
+                throw e;
+            }
+//            if (authElements.length == 2
+//                    && "Bearer".equals(authElements[0])) {
+//
+//            }
         }
 
         filterChain.doFilter(httpServletRequest, httpServletResponse);
